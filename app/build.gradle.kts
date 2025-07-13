9ad27a538a1965035fe6c3ba652c548ce577563a
Stirven0 <juliocesaramaya91@hotmail.com>
2025-07-12 20:09:10 -0500

    cargando el proyecto

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+
+plugins {
+    id("com.android.application")
+    
+}
+
+android {
+    namespace = "com.aa.datastructure"
+    compileSdk = 33
+    
+    defaultConfig {
+        applicationId = "com.aa.datastructure"
+        minSdk = 33
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        
+    }
+    
+}
+
+dependencies {
+
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
